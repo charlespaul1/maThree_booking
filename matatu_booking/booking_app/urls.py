c8e2f2185bcee5c8612c097b77f156779a00275d
charlespaul1 <charlespaulwabomba@gmail.com>
2024-03-12 17:52:33 +0300

    created view of register and temp

--- matatu_booking/booking_app/urls.py
+++ matatu_booking/booking_app/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    
     
     
 ]
--- matatu_booking/booking_app/views.py
+++ matatu_booking/booking_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 def home(request):
     # check to see if user is logging in
     if request.method == 'POST':
@@ -25,4 +27,25 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have logged out....')
     return redirect('home')
+    
+# registering new user
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate the user and then log them in
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            
+            user = authenticate(username=username, email=email, password=password)
+            # after authentication log them in
+            login(request, user)
+            messages.success(request, "You have succesfully registered welcome...")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
     
