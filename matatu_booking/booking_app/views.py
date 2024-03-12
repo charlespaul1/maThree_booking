f236d3f2d006bddd5547a5c425b428139336f34b
charlespaul1 <charlespaulwabomba@gmail.com>
2024-03-12 14:54:51 +0300

    added login feature

--- matatu_booking/booking_app/views.py
+++ matatu_booking/booking_app/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if user is logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        # authenticate the user
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,'login successfull')
+            return redirect('home')
+        else:
+            messages.success(request, 'Log in Error, Please try again....')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+#logout user
+def logout_user(request):
+    pass
+    
