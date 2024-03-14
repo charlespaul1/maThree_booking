151ad4c9f9e79ba84bab66541b10aa2fde975ad1
charlespaul1 <charlespaulwabomba@gmail.com>
2024-03-14 14:31:29 +0100

    created models for matatu route and bookings

--- matatu_booking/booking_app/models.py
+++ matatu_booking/booking_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class MatatuRoute(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    starting_point = models.CharField(max_length=50)
+    destination = models.CharField(max_length=50)
+    fare = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return (f'{self.first_name} - {self.last_name}')
+    
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    route = models.ForeignKey(MatatuRoute, on_delete=models.CASCADE)
+    departure_date = models.DateField()
+    departure_time = models.TimeField()
+    seat_number = models.IntegerField()
+    
+    def __str__(self):
+        return (f'{self.username} - {self.route.name} - Seat {self.seat_number}')
+    
